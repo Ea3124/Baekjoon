535ee1d4941864fdc153666c1a7984d1090bff6e
Ea3124 <leesj6717@naver.com>
2024-01-03 14:06:30 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -8,43 +8,17 @@ int main() {
     char count_max_char =0;
 
     for (int i = 0; i < c_word.length(); ++i) {
-        if (c_word[i] == 'c') {
-            if(c_word[i+1] == '='){
-                count_length--;
-            }
-            if(c_word[i+1] == '-'){
-                count_length--;
-            }
-        }
+        if (c_word[i] == 'c' && (c_word[i+1] == '=' || c_word[i+1] == '-')) count_length--;
 
-        if (c_word[i] == 'd') {
-            if(c_word[i+1] == 'z'&&c_word[i+2] =='='){
-                count_length--;
-            }
-            if(c_word[i+1] == '-'){
-                count_length--;
-            }
-        }
-        if (c_word[i] == 'l') {
-            if(c_word[i+1] == 'j'){
-                count_length--;
-            }
-        }
-        if (c_word[i] == 'n') {
-            if(c_word[i+1] == 'j'){
-                count_length--;
-            }
-        }
-        if (c_word[i] == 's') {
-            if(c_word[i+1] == '='){
-                count_length--;
-            }
-        }
-        if (c_word[i] == 'z') {
-            if(c_word[i+1] == '='){
-                count_length--;
-            }
-        }
+        if (c_word[i] == 'd' && ((c_word[i+1] == 'z'&&c_word[i+2] =='=')|| c_word[i+1] == '-')) count_length--;
+
+        if (c_word[i] == 'l' && c_word[i+1] == 'j') count_length--;
+
+        if (c_word[i] == 'n' && c_word[i+1] == 'j') count_length--;
+
+        if (c_word[i] == 's' && c_word[i+1] == '=') count_length--;
+
+        if (c_word[i] == 'z' && c_word[i+1] == '=') count_length--;
     }
 
     std::cout<<count_length;
