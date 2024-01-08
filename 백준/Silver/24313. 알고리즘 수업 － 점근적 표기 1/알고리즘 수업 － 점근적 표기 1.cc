54fd3e6603d10c729137a7c6e726dbd07e50b4ea
Ea3124 <leesj6717@naver.com>
2024-01-08 22:16:41 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    int a1,a0;
+    int c;
+    int n0, state;
+    std::cin>>a1>>a0;
+    std::cin>>c;
+    std::cin>>n0;
+    while(n0<=100) {
+        if (a1 * n0 + a0 > c * n0) {
+            state = 0;
+            break;
+        }
+        else state =1;
+        n0++;
+    }
+    if (state == 0) std::cout << 0;
+    else std::cout<<1;
+    return 0;
+}
