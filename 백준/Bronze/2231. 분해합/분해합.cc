b33c4e0efd60f8c7da691880a9573fdc1d071043
Ea3124 <leesj6717@naver.com>
2024-01-09 17:08:32 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,26 +1,15 @@
-
 #include <iostream>
-#include <string>
 
-int RecursiveTenFunction(std::string& x, int N) {
-    int powerOfTen = 1;
-    int Ret = N;
-    for (int i = 0; i < x.length(); i++) {
-        Ret += (N / powerOfTen) % 10;
-        powerOfTen *= 10;
-    }
-    return Ret;
-}
 int main() {
     int M, NMin;
-    std::string MM;
-    std::cin >> MM;
-    M = std::stoi(MM);
+    std::cin >> M;
     NMin = M; // M을 넘지 않는다.
     int N;
+
     for (int x = M; x >= 1; x--) {
         N=x;
-        if ((M == RecursiveTenFunction(MM,N)) && (N <= NMin)) NMin = N;
+
+        if ((M == N+(N/100000)%10+(N/10000)%10+(N/1000)%10+(N/100)%10+(N/10)%10+N%10) && (N <= NMin)) NMin = N;
     }
 
     if(NMin == M) std::cout<<0;
@@ -28,8 +17,3 @@ int main() {
 
     return 0;
 }
-
-/*
- M == N+N/100+(N/10)%10+N%10
-*/
-
