8eb42db95fa9b6a18a50e0c40904014c391b3144
Ea3124 <leesj6717@naver.com>
2024-01-13 01:06:39 +0900

    [Silver V] Title: 숫자 카드, Time: 628 ms, Memory: 5892 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,28 +1,36 @@
 #include <iostream>
-#include <unordered_set>
 #include <vector>
+#include <algorithm>
+void BinarySearch(std::vector<int>& L, int r, int low, int high){
+    if(low>high) std::cout<<0<<' ';
+    else {
+        int mid = (low + high) / 2;
+        int e = L.at(mid);
+        if (r == e) std::cout << 1 << ' ';
+        else if (r < e) BinarySearch(L, r, low, mid - 1);
+        else BinarySearch(L, r, mid + 1, high);
+    }
+}
 
 int main() {
-    int A, B, A_key;
-
+    int A, B;
     std::cin >> A;
-    std::unordered_set<int> card;
+    std::vector<int> card;
 
     for (int i = 0; i < A; i++) {
-        std::cin >> A_key;
-        card.insert(A_key);
+        int value;
+        std::cin >> value;
+        card.push_back(value);
     }
-
+    std::sort(card.begin(),card.end());
     std::cin >> B;
-    std::vector<int> MatchCard(B);
+    int MatchCard[B];
 
-    for (int &match : MatchCard) {
-        std::cin >> match;
+    for (int j = 0; j < B; j++) {
+        std::cin >> MatchCard[j];
     }
-
-    for (const int &match : MatchCard) {
-        std::cout << (card.find(match) != card.end()) << ' ';
+    for (int k=0;k<B;k++) {
+        BinarySearch(card, MatchCard[k],0,A-1);
     }
-
     return 0;
 }
