19ab9820233f24df6a7fea809f7fcd6e45046abe
Ea3124 <leesj6717@naver.com>
2024-03-05 11:00:15 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 80 ms, Memory: 6776 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <set>
+
+    int main() {
+        std::cin.tie(NULL);
+        std::cout.tie(NULL);
+        std::ios_base::sync_with_stdio(false);
+
+        int N;
+        std::cin >> N;
+
+        std::set<std::pair<int, int>> x_y;
+
+        for (int i = 0; i < N; i++) {
+            int a, b;
+            std::cin >> a >> b;
+            x_y.insert({b, a});
+        }
+
+        // x_y에 저장된 모든 쌍 출력 (선택적)
+        for (const auto& p : x_y) {
+            std::cout << p.second << " " << p.first << "\n";
+        }
+
+        return 0;
+    }
