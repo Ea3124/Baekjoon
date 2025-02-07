6c69a696ed2b0e738d540456df8600b2dc1d29e2
Ea3124 <leesj6717@naver.com>
2025-02-08 06:04:57 +0900

    [Silver II] Title: 에디터, Time: 132 ms, Memory: 20880 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -24,8 +24,6 @@ int main(void) {
         }
         else if(cursor =='L') {
             if (t != L.begin()) t--;
-            // 조건 변경
-            // qwx"
         }
         else if(cursor =='D') {
             if (t != L.end()) t++;
@@ -40,23 +38,3 @@ int main(void) {
 
     for(auto i : L) cout << i;
 }
-// abcd"
-// insert시, t 앞 추가, t는 그대로
-// erase시, t 삭제, t는 뒤에거 가리킴
-
-// 추가시, t가 첫 번째일시 push_front 아닐시 insert?
-// -> 이럴려면 1번
-
-// 1. t는 마지막 문자 뒤의 것을 가리키면서 시작 -> 계속 그렇게 되어야함
-
-// insert는 괜춘
-// erase가 좀 변경됨
-
-
-
-// 2. t는 마지막 문자를 가리키면서 시작 -> 계속 그렇게 되어야함
-
-// insert 변경
-// erase 그대로
-
-// 둘 다 안됨 ㄹㅈㄷ
