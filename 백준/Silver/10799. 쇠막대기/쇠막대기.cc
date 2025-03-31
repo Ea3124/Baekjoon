77155d735803055c83a27b00898868de3211fd65
Ea3124 <leesj6717@naver.com>
2025-03-31 11:26:19 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -5,7 +5,7 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int count=0, l=0, p=0;
+    int count=0, p=0;
     string s;
     cin>>s;
 
@@ -13,34 +13,26 @@ int main() {
 
     for(auto c : s){
         if(c=='('){
-            if(C.empty()) { C.push(c); 
-//                cout<<"pp: "<<p<<"count: "<<count<<'\n';
+            if(C.empty()) { C.push(c);
             }
             else if(C.top() ==')') {
-                C.push(c); 
-//                cout<<")p: "<<p<<"count: "<<count<<'\n';
+                C.push(c);
             }
             else if(C.top() =='(') {
                 p= p+1;
                 count= count+1;
-//                cout<<"+p: "<<p<<"count: "<<count<<'\n';
             }
         }
         else if(c==')'){
             if(C.top() =='(') {
                 C.push(c);
                 count = count + p;
-//                C.pop();
-//                cout<<"cp: "<<p<<"count: "<<count<<'\n';
             }
             else if(C.top() ==')') {
                 C.push(c);
                 p= p-1;
-//                C.pop();
-//                cout<<"-p: "<<p<<"count: "<<count<<'\n';
             }
         }
     }
     cout<<count;
-
     }
